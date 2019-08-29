417a6d1593ed6f9e69e6af520892b28eb246bacb
sairamravulakolu <sairam.ravulakolu@gmail.com>
2019-08-29 09:54:05 +0530

    Added allow getters in Account.java

--- src/main/java/com/reallabs/eams/model/Account.java
+++ src/main/java/com/reallabs/eams/model/Account.java
@@ -21,13 +21,12 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties(value={"createdAt","lastUpdate"})
+@JsonIgnoreProperties(value={"createdAt","lastUpdate"},allowGetters=true)
 public class Account implements Serializable {
 
 	/**
